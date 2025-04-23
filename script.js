a7b7a39d8ac812ad2a99a3e52d30f9608ef10612
irenep1127 <irenep1127@gmail.com>
2025-04-23 22:54:40 +0000

    Version 2

--- script.js
+++ script.js
@@ -36,15 +36,15 @@ function main() {
   let words = string.split(" ");
 
   //saves current script to local storage
-  saveToLocalStorage(string);
+  //saveToLocalStorage(string);
 
-  let pos = 1;
+  let pos = 0;
   let hideWordEvery = document.querySelector("#skip-distance").value || 2;
   let htmlOutput = "";
   for (let word of words) {
     pos++;
     // skips if just whitespace
-    if (word.match(/\n/g)) continue;
+    if (word.trim() === "") continue;
     if (pos % hideWordEvery == 0) {
       let endingPunctuation = "";
       if (word.at(-1) == "." || word.at(-1) == ",") {
@@ -84,22 +84,23 @@ function showScriptEditor() {
 }
 
 
-function saveToLocalStorage(text) {
-  let parsedText = parseText(input);
-  localStorage.setItem(a, paresdTextArray);
-}
+// function saveToLocalStorage(text) {
+//   let parsedTextArray = parseText(text);
+//   localStorage.setItem(a, parsedTextArray);
+// }
 
 
 function parseText(text) {
-  let words = text.split("\\n");
+  let words = text.split("\n");
   let parsedText = "";
   let parsedTextArray;
+  let hideWordEvery = document.querySelector("#skip-distance").value || 2;
   for (let word of words) {
     if (word.match(/\n/g)) continue;
-    parsedText `<span class="hidden">${word}</span> `;
+    parsedText += `<span class="hidden">${word}</span> `;
   }
 
-  paresdTextArray = parsedText.split(" ");
+  parsedTextArray = parsedText.split(" ");
   for (let i = 0; i < parsedTextArray.length; i++) {
     if (parsedTextArray[i].match(/\n/g)) continue;
     if (i % hideWordEvery == 0) {

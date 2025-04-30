e6628dd1a4b184da3f71ed77692e99b211ca8ed0
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2025-04-29 21:57:15 -0400

    Style fixes

--- script.js
+++ script.js
@@ -1,206 +1,208 @@
 let savedScriptNames;
 const SaveManager = {
-  saveScript(name, linesArray) {
-    let data = {
-      name: name,
-      lines: linesArray,
-      skipDistance: document.getElementById("skip-distance").value,
-      character: document.getElementById("character").value,
-      lastEdit: Date.now()
-    }
-    localStorage.setItem("scriptmemorizer-" + name, JSON.stringify(data));
-    console.log(savedScriptNames);
-    savedScriptNames.push(name);
-    localStorage.setItem("scriptmemorizer:scriptnames", JSON.stringify(savedScriptNames));
-  },
-  getScript(name) {
-    return JSON.parse(localStorage.getItem("scriptmemorizer-" + name));
-  },
-  async getScriptNames() {
-    let a = JSON.parse(localStorage.getItem("scriptmemorizer:scriptnames"));
-    console.log(a);
-    return a;
-  }
+   saveScript(name, linesArray) {
+      let data = {
+         name: name,
+         lines: linesArray,
+         skipDistance: document.getElementById("skip-distance").value,
+         character: document.getElementById("character").value,
+         lastEdit: Date.now()
+      }
+      localStorage.setItem("scriptmemorizer-" + name, JSON.stringify(data));
+      savedScriptNames.push(name);
+      localStorage.setItem("scriptmemorizer:scriptnames", JSON.stringify(savedScriptNames));
+   },
+   getScript(name) {
+      return JSON.parse(localStorage.getItem("scriptmemorizer-" + name));
+   },
+   async getScriptNames() {
+      let a = JSON.parse(localStorage.getItem("scriptmemorizer:scriptnames"));
+      return a;
+   }
 }
 
 init();
-
-
 async function init() {
-  savedScriptNames = await SaveManager.getScriptNames();
+   savedScriptNames = await SaveManager.getScriptNames();
 
-  if (!savedScriptNames) {
-    savedScriptNames = [];
-    localStorage.setItem("scriptmemorizer:scriptnames", JSON.stringify(savedScriptNames));
-  }
+   if (!savedScriptNames) {
+      savedScriptNames = [];
+      localStorage.setItem("scriptmemorizer:scriptnames", JSON.stringify(savedScriptNames));
+   }
 
-  let list = document.querySelector(".saved-scripts");
-  console.log(savedScriptNames);
-  savedScriptNames.forEach(name => {
-    let label = document.createElement("li");
-    label.addEventListener("click", () => {
-        showOnPage(name);
-        document.querySelector(".active")?.classList.remove("active");
-        label.classList.add("active");
-    });
-    label.addEventListener("change", () => { 
-        console.log("test1");
-        SaveManager.saveScript(label.name, document.getElementById("input").value);
-    });
-    label.textContent = name;
-    list.appendChild(label);
-  });
+   let list = document.querySelector(".saved-scripts");
+   savedScriptNames.forEach(name => {
+      let label = document.createElement("li");
+      label.addEventListener("click", () => {
+         showOnPage(name);
+         document.querySelector(".active")?.classList.remove("active");
+         label.classList.add("active");
+      });
+      label.addEventListener("change", () => {
+         SaveManager.saveScript(label.name, document.getElementById("input").value);
+      });
+      label.textContent = name;
+      list.appendChild(label);
+   });
 
 }
-
-
 
 
 
 function main(splitter) {
-  let input = document.querySelector("#input").value;
-  let string = input == "" ? "here is a piece of example text." : input;
-  let words = string.split(splitter);
-  
+   let input = document.querySelector("#input").value;
+   let string = input;
+   if (string.length < 1) return;
+   let words = string.split(splitter);
 
-  alert(typeof words);
-
-
-  let pos = 0;
-  let hideWordEvery = document.querySelector("#skip-distance").value || 2;
-  let htmlOutput = "";
-  for (let word of words) {
-    pos++;
-    // skips if just whitespace
-    if (word.trim() === "") continue;
-    if (pos % hideWordEvery == 0) {
-      let endingPunctuation = "";
-      if (word.at(-1) == "." || word.at(-1) == ",") {
-        endingPunctuation = word.at(-1);
-        word = word.substring(0, word.length - 1);
+   let pos = 0;
+   let hideWordEvery = document.querySelector("#skip-distance").value || 2;
+   let htmlOutput = "";
+   for (let word of words) {
+      pos++;
+      // skips if just whitespace
+      if (word.trim() === "") continue;
+      if (pos % hideWordEvery == 0) {
+         let endingPunctuation = "";
+         if (word.at(-1) == "." || word.at(-1) == ",") {
+            endingPunctuation = word.at(-1);
+            word = word.substring(0, word.length - 1);
+         }
+         htmlOutput += `<span class="hidden">${word}</span>${endingPunctuation} `;
       }
-      htmlOutput += `<span class="hidden">${word}</span>${endingPunctuation} `;
-    }
-    else {
-      htmlOutput += word + " ";
-    }
-  }
+      else {
+         htmlOutput += word + " ";
+      }
+   }
 
-  document.querySelector(".output").innerHTML = htmlOutput;
-  document.getElementById("input").style.visibility = "hidden";
+   document.querySelector(".output").innerHTML = htmlOutput;
+   hideScriptEditor();
 }
 
 function showAll() {
-  let hiddenWords = document.querySelectorAll(".hidden");
-  for (let element of hiddenWords) {
-    element.classList.add("shown");
-  }
+   let hiddenWords = document.querySelectorAll(".hidden");
+   for (let element of hiddenWords) {
+      element.classList.add("shown");
+   }
 }
 
 function hideAll() {
-  let shownWords = document.querySelectorAll(".shown");
-  for (let element of shownWords) {
-    element.classList.remove("shown");
-  }
+   let shownWords = document.querySelectorAll(".shown");
+   for (let element of shownWords) {
+      element.classList.remove("shown");
+   }
 
 }
 //Shows the script editor after it has disappeared
-function showScriptEditor() {
-  document.getElementById("input").style.visibility = "visible";
+function toggleScriptEditor() {
+   if (document.querySelector("#input").style.height == "2rem") {
+      showScriptEditor();
+   }
+   else hideScriptEditor();
+}
 
+function showScriptEditor() {
+   document.querySelector("#input").style.height = "15rem";
+   document.querySelector("#input").style.opacity = "1";
+   document.querySelector("#input").style.pointerEvents = "auto";
+}
+
+function hideScriptEditor() {
+   document.querySelector("#input").style.height = "2rem";
+   document.querySelector("#input").style.opacity = "0";
+   document.querySelector("#input").style.pointerEvents = "none";
 }
 
 
 
 function parseText() {
-  let lines = document.querySelector("#input").value.split("\n");
-  
-  let parsedText = "";
-  let parsedTextArray;
-  let hideWordEvery = document.querySelector("#skip-distance").value || 2;
-  
-  for (let line of lines) {
-    if (line.match(/\n/g)) continue;
-    parsedText += `<span class="hidden">${wordline}</span> `;
-  }
-  alert("lines");
-  parsedTextArray = parsedText.split(" ");
-  for (let i = 0; i < parsedTextArray.length; i++) {
-    if (parsedTextArray[i].match(/\n/g)) continue;
-    if (i % hideWordEvery == 0) {
-      parsedTextArray[i] = `<span class="hidden">${parsedTextArray[i]}</span>`;
-    }
-  }
-  document.querySelector(".output").innerHTML = htparseTextArray;
-  document.getElementById("input").style.visibility = "hidden";
-  
-  return parsedTextArray;
+   let lines = document.querySelector("#input").value.split("\n");
+
+   let parsedText = "";
+   let parsedTextArray;
+   let hideWordEvery = document.querySelector("#skip-distance").value || 2;
+
+   for (let line of lines) {
+      if (line.match(/\n/g)) continue;
+      parsedText += `<span class="hidden">${wordline}</span> `;
+   }
+   alert("lines");
+   parsedTextArray = parsedText.split(" ");
+   for (let i = 0; i < parsedTextArray.length; i++) {
+      if (parsedTextArray[i].match(/\n/g)) continue;
+      if (i % hideWordEvery == 0) {
+         parsedTextArray[i] = `<span class="hidden">${parsedTextArray[i]}</span>`;
+      }
+   }
+   document.querySelector(".output").innerHTML = htparseTextArray;
+   document.getElementById("input").style.visibility = "hidden";
+
+   return parsedTextArray;
 
 }
 
 // when called, function will take in the name of the script then display it in the output
 function showOnPage(name) {
-  console.log("I never wanna give up, I never let you down");
-  let script = SaveManager.getScript(name);
-  let lines = script.lines;
-  let htmlOutput = "";
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i].match(/\n/g)) continue;
-    htmlOutput += lines[i] + " ";
-  }
-  document.querySelector(".output").innerHTML = htmlOutput;
-  document.getElementById("input").value = htmlOutput;
+   let script = SaveManager.getScript(name);
+   let lines = script.lines;
+   let htmlOutput = "";
+   for (let i = 0; i < lines.length; i++) {
+      if (lines[i].match(/\n/g)) continue;
+      htmlOutput += lines[i] + " ";
+   }
+   document.querySelector(".output").innerHTML = htmlOutput;
+   document.getElementById("input").value = htmlOutput;
 
-  
+
 }
 
 //creates new sidebar with the new script
-function showSavedScript(){
-  //clears the sidebar
-  let list = document.querySelector("saved-scripts");
-  list.innerHTML = "";
-  
-  //adds the add new script button
-  let addScriptButton = document.createElement("li");
-  addScriptButton.classList.add("new-script");
-  addScriptButton.textContent = "+ new script";
-  addScriptButton.onclick = addNewScript;
-  list.appendChild("addScriptButton");
-  //let newScript = document.createElement("li");
-  
-  savedScriptNames.array.forEach(name => {
-    if(name === "+ new script")return;
-    let newScript = document.createElement("li");
-    newScript.textContent = name;
-    newScript.addEventListener("click", showOnPage(name));
-    list.appendChild("newScript");
-  });
+function showSavedScript() {
+   //clears the sidebar
+   let list = document.querySelector("saved-scripts");
+   list.innerHTML = "";
+
+   //adds the add new script button
+   let addScriptButton = document.createElement("li");
+   addScriptButton.classList.add("new-script");
+   addScriptButton.textContent = "+ new script";
+   addScriptButton.onclick = addNewScript;
+   list.appendChild("addScriptButton");
+   //let newScript = document.createElement("li");
+
+   savedScriptNames.array.forEach(name => {
+      if (name === "+ new script") return;
+      let newScript = document.createElement("li");
+      newScript.textContent = name;
+      newScript.addEventListener("click", showOnPage(name));
+      list.appendChild("newScript");
+   });
 }
 
 //adds another label on the side
 function addNewScript() {
-  let name = prompt("What would you like to name your chat");
-  if (!name) return;
+   let name = prompt("What would you like to name your chat");
+   if (!name) return;
 
-  if (savedScriptNames.includes(name)) {
-    alert("This name already exists");
+   if (savedScriptNames.includes(name)) {
+      alert("This name already exists");
 
-    let num = 1;
-    let baseName = name;
-    for (let i = 0; i < savedScriptNames.length; i++) {
-      if (savedScriptNames[i] == name) { num++ };
-    }
-    name = baseName + "" + num;
-    SaveManager.saveScript(name, document.getElementById("input").value);
-  } else {
-    //adds the new script as an object
-    SaveManager.saveScript(name, document.getElementById("input").value);
-  }
+      let num = 1;
+      let baseName = name;
+      for (let i = 0; i < savedScriptNames.length; i++) {
+         if (savedScriptNames[i] == name) { num++ };
+      }
+      name = baseName + "" + num;
+      SaveManager.saveScript(name, document.getElementById("input").value);
+   } else {
+      //adds the new script as an object
+      SaveManager.saveScript(name, document.getElementById("input").value);
+   }
 
 
-  //list of labels on the side
-  let list = document.querySelector(".saved-scripts");
-  let newScript = document.createElement("li");
-  newScript.textContent = name;
-  list.appendChild(newScript);
+   //list of labels on the side
+   let list = document.querySelector(".saved-scripts");
+   let newScript = document.createElement("li");
+   newScript.textContent = name;
+   list.appendChild(newScript);
 }
